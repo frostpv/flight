602de18e5a110106526394d58d88d73bd2831375
Laszlo Petechuk <frostpv@gmail.com>
2020-11-11 20:34:39 +0200

    added Airplane model

--- src/main/java/com/flight/demo/model/Airplane.java
+++ src/main/java/com/flight/demo/model/Airplane.java
@@ -1,7 +1,16 @@
 package com.flight.demo.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Airplane {
     private Long id;
     private AirplaneCharacteristics airplaneCharacteristics;
-
+    private TemporaryPoint temporaryPoint;
+    List <Flight> flights;
 }
